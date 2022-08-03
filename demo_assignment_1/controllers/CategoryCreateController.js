3e8332e9e63877fa3c53841df2d11919dcdad2e8
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-08-03 11:06:51 +0700

    Create http post

--- demo_assignment_1/controllers/CategoryCreateController.js
+++ demo_assignment_1/controllers/CategoryCreateController.js
@@ -1,3 +1,20 @@
-window.CategoryCreateController = function ($scope) {
+window.CategoryCreateController = function ($scope, $http, $location) {
     $scope.categoryName = '';
+    // Hàm được gọi khi click btn submit form tạo category
+    $scope.onSubmit = function () {
+        $http.post(
+            categoryAPI, // API url
+            { name: $scope.categoryName } // dữ liệu
+        ).then(
+            function (response) {
+                console.log(response);
+                if (response.status === 201) {
+                    $location.path('categories');
+                }
+            },
+            function (errors) {
+                console.log(errors);
+            }
+        );
+    }
 };
