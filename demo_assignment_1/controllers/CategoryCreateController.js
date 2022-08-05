bcfa9b1afec5228e6af48889a2649d0042ec4616
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-08-05 10:36:57 +0700

    Upload file base64

--- demo_assignment_1/controllers/CategoryCreateController.js
+++ demo_assignment_1/controllers/CategoryCreateController.js
@@ -3,6 +3,19 @@ window.CategoryCreateController = function (
 ) {
     // Vẫn tiến hành khởi tạo giá trị mặc định cho categoryName bên input
     $scope.categoryName = '';
+    $scope.imageUrl = '';
+
+    $scope.onChangeImage = function ($event) {
+        console.log($event.target.files); // có thể upload nhiều file nên files là 1 mảng
+        var file = $event.target.files[0]; // nếu upload 1 ảnh thì lấy file ở vị trí 0
+        var reader = new FileReader(); // sử dụng đối tượng FileReader của JS để đọc file
+        reader.readAsDataURL(file); // truyền file vào để đọc
+        reader.onload = function () { // khi bắt đầu đọc sẽ gán kết quả đọc cho imageUrl
+            $scope.imageUrl = reader.result;
+
+            console.log($scope.imageUrl);
+        }
+    }
 
     // Kết hợp chức năng sửa category
     var id = $routeParams.id;
@@ -12,6 +25,7 @@ window.CategoryCreateController = function (
         $http.get(`${categoryAPI}/${id}`).then(
             function(response) {
                 if (response.status === 200) {
+                    console.log(response);
                     // response.data = {id: xxx, name: "YYYY"};
                     // Gán lại giá trị cho input hiển thị sửa
                     $scope.categoryName = response.data.name;
@@ -21,14 +35,29 @@ window.CategoryCreateController = function (
                 console.log(errors);
             }
         );
-
     }
 
     // Hàm được gọi khi click btn submit form tạo category
     $scope.onSubmit = function () {
+        if (id) {
+            return $http.put(
+                `${categoryAPI}/${id}`,
+                { name: $scope.categoryName}
+            ).then(
+                function(response) {
+                    if (response.status === 200) {
+                        $location.path('categories');
+                    }
+                },
+                function(errors) {
+                    console.log(errors);
+                }
+            )
+        }
+
         $http.post(
             categoryAPI, // API url
-            { name: $scope.categoryName } // dữ liệu
+            { name: $scope.categoryName, imageUrl: $scope.imageUrl } // dữ liệu
         ).then(
             function (response) {
                 console.log(response);
