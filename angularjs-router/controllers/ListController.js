f270a4d9401045f09deadb7fc3915bd0120b8d95
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2023-02-10 15:17:29 +0700

    API

--- angularjs-router/controllers/ListController.js
+++ angularjs-router/controllers/ListController.js
@@ -1,3 +1,23 @@
-var ListController = function ($scope) {
-    $scope.title = "TRANG DS DA TACH CONTROLLER";
+var ListController = function ($scope, $http) {
+    $scope.api = 'https://63e5fb6483c0e85a868a560d.mockapi.io/';
+    $scope.title = "Danh sách người dùng";
+    $scope.nguoi_dung = [];
+    $scope.isLoading = true;
+    // 1. Call API đã định nghĩa ở trang mockAPI.io
+    // $http sẽ có những phương thức như get, post, put... để call API
+    $http.get($scope.api + 'users').then(
+        function (res) {
+            if (res.status == 200) {
+                $scope.isLoading = false;
+                $scope.nguoi_dung = res.data;
+            }
+        }, // khi chờ dữ liệu thành công
+        function (err) {
+            console.log(err);
+        } // khi chờ dữ liệu nhưng thất bại
+    )
+
+    // 2. gán giá trị đã nhận được cho $scope
+    // 3. Ở view sẽ nhận giá trị và hiển thị
+
 }
